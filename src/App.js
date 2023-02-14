11aa459a91ca872a56443da0ecbba6f95ee0356e
Lucas Knupp <lucas_knupp@outlook.com>
2023-02-14 15:46:02 -0300

    sidebar component developed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import SidebarComp from "./components/sidebar/SidebarComp";
 import TopbarComp from "./components/topbar/TopbarComp";
+import './app.css';
 
 function App() {
-  return <div><TopbarComp /></div>;
+  return (
+    <div>
+      <TopbarComp />
+      <div className="container">
+          <SidebarComp />
+          <div className="others">other pages</div>
+      </div>
+    </div>
+  ) 
 }
 
 export default App;
--- src/components/sidebar/SidebarComp.jsx
+++ src/components/sidebar/SidebarComp.jsx
@@ -1,8 +1,85 @@
+import { LineStyle, Timeline, TrendingUp, Person, Inventory2, MonetizationOn, Assessment,
+Email, Feedback, Message, ManageAccounts, Report } from '@mui/icons-material';
 import "./sidebar.css"
 
 function SidebarComp() {
   return (
-    <div>SidebarComp</div>
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <LineStyle className='sidebarIcon'/>
+              Home
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className='sidebarIcon'/>
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <TrendingUp className='sidebarIcon' />
+              Sales
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <Person className='sidebarIcon'/>
+              Users
+            </li>
+            <li className="sidebarListItem ">
+              <Inventory2 className='sidebarIcon'/>
+              Products
+            </li>
+            <li className="sidebarListItem">
+              <MonetizationOn className='sidebarIcon'/>
+              Transactions
+            </li>
+            <li className="sidebarListItem">
+              <Assessment className='sidebarIcon' />
+              Reports
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <Email className='sidebarIcon'/>
+              Mail
+            </li>
+            <li className="sidebarListItem">
+              <Feedback className='sidebarIcon'/>
+              Feedback
+            </li>
+            <li className="sidebarListItem">
+              <Message className='sidebarIcon' />
+              Messages
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <ManageAccounts className='sidebarIcon'/>
+              Manage
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className='sidebarIcon'/>
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <Report className='sidebarIcon' />
+              Reports
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
